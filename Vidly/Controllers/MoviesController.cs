9c037ddbb8d79c1dc57e5a8677c2f9aa9e0729c7
Diego Kuwahara de Castro <diegookc@hotmail.com>
2018-10-08 22:14:44 -0300

    Primeiro Commit

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.ViewModels;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "xerek" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            var viewResult = new ViewResult();
+            return View(viewModel);
+        }
+
+        public ActionResult CustomersList()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "John Smith" },
+                new Customer { Name = "Mary Williams"}
+            };
+
+            var modelo = new CustomersListViewModel
+            {
+                Customers = customers
+            };
+            return View(modelo);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
